fef66738ecf2cf806eb5c59a72aed27a680cc401
Manish Singh <manishsingh10895@gmail.com>
2019-08-04 08:33:27 +0530

    config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     "development": {
-        // "DB": "mongodb://localhost:27017/noveltycoin"
-        "DB": "mongodb://manish:Term!nator@ds259347.mlab.com:59347/noveltycoin"
+        "DB": "mongodb://localhost:27017/noveltycoin"
+        // "DB": "mongodb://manish:Term!nator@ds259347.mlab.com:59347/noveltycoin"
     },
     "production": {
         "DB": "mongodb://manish:Term!nator@ds259347.mlab.com:59347/noveltycoin"
